0f5abe1132cc19582039742265417ab6db1bd77d
lwtsn <luke.connor.watson@gmail.com>
2026-01-06 10:16:59 +0800

    chore: remove event bus

--- pkg/modules.go
+++ pkg/modules.go
@@ -4,18 +4,17 @@ import (
 	"github.com/backtesting-org/kronos-sdk/pkg/activity"
 	"github.com/backtesting-org/kronos-sdk/pkg/adapters"
 	"github.com/backtesting-org/kronos-sdk/pkg/analytics"
-	"github.com/backtesting-org/kronos-sdk/pkg/events"
+	"github.com/backtesting-org/kronos-sdk/pkg/data/ingestors"
+	"github.com/backtesting-org/kronos-sdk/pkg/data/stores"
 	"github.com/backtesting-org/kronos-sdk/pkg/executor"
 	"github.com/backtesting-org/kronos-sdk/pkg/health"
 	"github.com/backtesting-org/kronos-sdk/pkg/inference/features"
-	"github.com/backtesting-org/kronos-sdk/pkg/ingestors"
 	"github.com/backtesting-org/kronos-sdk/pkg/lifecycle"
 	"github.com/backtesting-org/kronos-sdk/pkg/plugin"
 	"github.com/backtesting-org/kronos-sdk/pkg/profiling"
 	"github.com/backtesting-org/kronos-sdk/pkg/registry"
 	"github.com/backtesting-org/kronos-sdk/pkg/runtime"
 	"github.com/backtesting-org/kronos-sdk/pkg/signal"
-	"github.com/backtesting-org/kronos-sdk/pkg/stores"
 	"go.uber.org/fx"
 )
 
@@ -23,7 +22,6 @@ var Module = fx.Options(
 	activity.Module,
 	adapters.Module,
 	analytics.Module,
-	events.Module,
 	features.Module,
 	health.Module,
 	ingestors.Module,
