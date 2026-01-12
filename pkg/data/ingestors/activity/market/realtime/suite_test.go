22aa04dd520ef0ed8865b9a8971f0a9869011672
lwtsn <luke.connor.watson@gmail.com>
2026-01-12 13:51:13 +0800

    feat(test) change real time ingestor to integration style test

---
+++ pkg/data/ingestors/activity/market/realtime/suite_test.go
@@ -0,0 +1,13 @@
+package realtime
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestRealtime(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Realtime Ingestor Suite")
+}
